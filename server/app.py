aa45e38c8c6c72084afd6776ed17d813ca7227d6
Yin Min Aung <ivannoxic632@gmail.com>
2024-09-20 07:35:58 +0800

    create migrations in database, add userdetails fetching endpoints

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_migrate import Migrate
@@ -22,24 +23,115 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class UserDetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Mode: manual, auto, away
+    mode = db.Column(db.String(20), nullable=False, default='auto')
+        
+    #curtina and light status
+    curtain_status = db.Column(db.Boolean, nullable=True)
+    light_status = db.Column(db.Boolean, nullable=True)
+        
+    # Timestamps for last updates
+    curtain_last_updated = db.Column(db.DateTime, nullable=False , default = "Not updated")
+    light_last_updated = db.Column(db.DateTime, nullable=False ,default = "Not updated")
+        
+    # For auto mode: light threshold
+    # can change this later based on what the light sensor gives
+    light_threshold = db.Column(db.Float, nullable=True, default=0.0) 
+
+    # For away mode: specify if curtain or light is in auto mode
+    curtain_auto = db.Column(db.Boolean, nullable=True)
+    light_auto = db.Column(db.Boolean, nullable=True)
+
+    def __repr__(self):
+        return f'<UserDetails {self.username} - Mode: {self.mode}>'
+    
+
+@app.route('/add_user_details', methods=['POST'])
+def add_user_details():
+    data = request.get_json()  # Get JSON data from request
+
+    # Extracting data from request, some fields are optional (nullable)
+    mode = data.get('mode', 'auto')  # Default to 'auto' if not provided
+    curtain_status = data.get('curtain_status')  # Nullable, can be omitted
+    light_status = data.get('light_status')      # Nullable, can be omitted
+    curtain_last_updated = data.get('curtain_last_updated', None)
+    light_last_updated = data.get('light_last_updated', None)
+    light_threshold = data.get('light_threshold', 0.0)  # Default to 0.0
+    curtain_auto = data.get('curtain_auto')  # Nullable, can be omitted
+    light_auto = data.get('light_auto')      # Nullable, can be omitted
+
+    # Handle the case where timestamps aren't provided
+    if curtain_last_updated is None:
+        curtain_last_updated = datetime.now()
+    else:
+        curtain_last_updated = datetime.strptime(curtain_last_updated, '%Y-%m-%d %H:%M:%S')
+
+    if light_last_updated is None:
+        light_last_updated = datetime.now()
+    else:
+        light_last_updated = datetime.strptime(light_last_updated, '%Y-%m-%d %H:%M:%S')
+
+    # Creating a new UserDetails object
+    new_user_details = UserDetails(
+        mode=mode,
+        curtain_status=curtain_status,      
+        light_status=light_status,        
+        curtain_last_updated=curtain_last_updated,
+        light_last_updated=light_last_updated,
+        light_threshold=light_threshold,
+        curtain_auto=curtain_auto,        
+        light_auto=light_auto       
+    )
+
+    # Save the new UserDetails to the database
+    db.session.add(new_user_details)
+    db.session.commit()
+
+    return jsonify({"message": "User details created successfully!", "id": new_user_details.id}), 201
+
+
+@app.route('/userdetails/<int:user_id>', methods=['GET'])
+def get_userdetails(user_id):
+    # Query the UserDetails table for the user with the given user_id
+    user = UserDetails.query.filter_by(id=user_id).first()
+
+    # If user is not found, return a 404 error
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    # If user is found, return their details
+    user_details = {
+        "id": user.id,
+        "mode": user.mode,
+        "curtain_status": user.curtain_status,
+        "light_status": user.light_status,
+        "curtain_last_updated": user.curtain_last_updated.strftime('%Y-%m-%d %H:%M:%S') if user.curtain_last_updated else None,
+        "light_last_updated": user.light_last_updated.strftime('%Y-%m-%d %H:%M:%S') if user.light_last_updated else None,
+        "light_threshold": user.light_threshold,
+        "curtain_auto": user.curtain_auto,
+        "light_auto": user.light_auto
+    }
+
+    return jsonify(user_details), 200
     
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    data = request.get_json()  # Get data sent via JSON
+    data = request.get_json()
     username = data.get('username')
     email = data.get('email')
 
-    # Ensure both username and email are provided
     if not username or not email:
         return jsonify({"error": "Username and email are required"}), 400
 
-    # Check if user already exists
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
         return jsonify({"error": "User already exists"}), 409
 
-    # Add the new user
     new_user = User(username=username, email=email)
     db.session.add(new_user)
     db.session.commit()
@@ -48,7 +140,7 @@ def add_user():
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = User.query.all()  # Get all users from the database
+    users = User.query.all()
     db.session.remove()
     user_list = [{"id": user.id, "username": user.username, "email": user.email} for user in users]
 
